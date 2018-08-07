414be23866de1ba4914043632b53adbed3393c37
destinybbrianna <destinybbrianna@gmail.com>
2018-08-07 20:06:34 +0000

    automatically backed up by learn

---
+++ fizz_buss.rb
@@ -0,0 +1,7 @@
+
+  # if the number is divisible by 3 = > fizz
+   # if the number is divisible by 5 = > buzz 
+     # if the number is divisible by 3 & h = > fizzbuzz
+   end
+   
+def fizzbuzz(num)
