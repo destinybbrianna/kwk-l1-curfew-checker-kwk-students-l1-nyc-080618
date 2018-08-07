20605a605b03dc6067920cd0aa0dfa8cc5863a0b
destinybbrianna <destinybbrianna@gmail.com>
2018-08-07 20:30:40 +0000

    automatically backed up by learn

--- fizz_buss.rb
+++ fizz_buss.rb
@@ -5,11 +5,13 @@
 
    
 def fizzbuzz(num)
-  if num % 3 == 0 
+  if num % 3 == 0 && num % 5 != 0
     puts "fizz"
     elsif num % 5 == 0 
     puts "buzz"
-    elsif num 3 num 5 
+    elsif num % 3 == 0 &&  num 5 
     puts "fizzbuzz"
   end
 end
+
+fizzbuzz(15)
